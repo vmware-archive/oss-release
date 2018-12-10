175e59a2ecbc0c0ee49cfd2e0a327f381cd49e3f
Ch3LL <megan.wilhite@gmail.com>
2018-12-10 10:16:41 -0500

    Add ENV variables for salt scripts

--- branch-updates/salt.sh
+++ branch-updates/salt.sh
@@ -3,18 +3,25 @@
 # This script pulls any new changes from Saltstack/salt.git to local repo
 # And then pushes those changes to rallytime/salt.git.
 
-cd ~/SaltStack/salt/
+for env_var in "${SALT_REPO}" "${ORIGIN}"; do
+  if [[ -z "${env_var}" ]] ; then
+      echo "Missing environment variable"
+      exit 1;
+  fi
+done
+
+cd ${SALT_REPO}
 
 # Check if there are any unstaged changes
-if [ -n "$(git status --porcelain)" ]; then 
+if [ -n "$(git status --porcelain)" ]; then
     git status
     echo "\nChanges present - Exiting.";
-else 
+else
     # Update all relevant, constant branches
     for branch in 2017.7 2018.3 fluorine develop
     do
         git checkout $branch
         git pull upstream $branch
-        git push rallytime $branch
+        git push ${ORIGIN} $branch
     done
 fi
